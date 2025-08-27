c55a688ec5d7b66533a2c699326873bde322175e
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2025-08-27 01:42:36 -0400

    Update AssetBundleAssetReference.cs

--- Runtime/AssetBundleAssetReference.cs
+++ Runtime/AssetBundleAssetReference.cs
@@ -6,7 +6,7 @@ using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 #endif
 
-namespace AssetBundleAssetReference.Runtime
+namespace com.raiden.assetbundleassetreference.Runtime
 {
     [System.Serializable]
     public class AssetBundleAssetReference : ISerializationCallbackReceiver
